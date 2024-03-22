23043dd2f603a12bbab7c3cd6ba0d9c684bc12a7
Ashwin Sunuwar <ashwinsunuwarr2@gmail.com>
2024-03-22 13:37:12 +0545

    appwrite db tanstack/react query context api

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import SIgnupForm from "./_auth/forms/SIgnupForm";
 import SigninForm from "./_auth/forms/SigninForm";
 import RootLayout from "./_root/RootLayout";
 import { Home } from "./_root/pages";
-import "./global.css";
+
 import { Routes, Route } from "react-router-dom";
+import { Toaster } from "@/components/ui/toaster";
 
 const App = () => {
   return (
@@ -21,6 +22,7 @@ const App = () => {
           <Route index element={<Home />} />
         </Route>
       </Routes>
+      <Toaster/>
     </main>
   );
 };
